1c3feab639e035c99032634f5dd35122ff3f0fbd
jawad-ul-hassan <intelligentjawad@gmail.com>
2022-09-04 16:46:01 +0500

    working on quotes page

---
+++ pages/quote.js
@@ -0,0 +1,11 @@
+const quote = () => {
+  return (
+    <div className="quote">
+      <header className="quote-header">
+        <span className="quote-header-close"></span>
+      </header>
+    </div>
+  );
+};
+
+export default quote;
