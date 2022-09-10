60db7bdbcaec0c4c91489851a493c2c547e76336
Atabic <atabic14@gmail.com>
2022-09-10 15:50:09 +0500

    next config change

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 
 module.exports = {
   reactStrictMode: true,
   env: {
-    apiURL: 'http://localhost:8000',
-    GOOGLE_CLIENT_ID: '65308577080 - 60l41rocpoun6rr743bdti7h7kj3j4ge.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: 'GOCSPX - pmyIFfUjx8pHAq8xBWaxIpAjpWem',
+    apiURL: process.env.apiURL,
+    GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET,
+    
+    FACBOOK_CLIENT_ID : process.env.FACBOOK_CLIENT_ID,
+    FACEBOOK_CLIENT_SECRET : process.env.FACEBOOK_CLIENT_SECRET,
 
-    FACBOOK_CLIENT_ID: '757409398865003',
-    FACEBOOK_CLIENT_SECRET: '6aa51d49732921140358e5471b9c3c9d',
-
-    LINKEDIN_CLIENT_ID: '771yejpnjshh2u',
-    LINKEDIN_CLIENT_SECRET: 'weXPMFNcmarBDboM',
+    LINKEDIN_CLIENT_ID: process.env.LINKEDIN_CLIENT_ID,
+    LINKEDIN_CLIENT_SECRET: process.env.LINKEDIN_CLIENT_SECRET,
   }
 }
