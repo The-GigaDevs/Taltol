368dbb61ab01f2e0b7d162e1f1b0f3fdab89dd7f
Atabic <atabic14@gmail.com>
2022-09-16 05:00:53 +0500

    nextauth fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -22,8 +22,9 @@ export default NextAuth({
             clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
         }),
         TwitterProvider({
-            clientId: process.env.TWITTER_ID,
-            clientSecret: process.env.TWITTER_SECRET,
+            clientId: process.env.TWITTER_CLIENT_ID,
+            clientSecret: process.env.TWITTER_CLIENT_SECRET,
+            version: "2.0"
         }),
         CredentialProvider({
             name: "Credentials",
