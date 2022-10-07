cb5fa1bf204990a9c546ed74b98d044749cab452
jawad-ul-hassan <intelligentjawad@gmail.com>
2022-10-07 23:53:07 +0500

    admin-topics page added

---
+++ pages/admin-topics.js
@@ -0,0 +1,15 @@
+import AdminHeader from '../components/admin/AdminHeader';
+import AdminMainSort from '../components/admin/AdminMainSort';
+import AdminQuotes from '../components/admin/AdminQuotes';
+import AdminTopics from '../components/admin/AdminTopics';
+
+const admin = () => {
+  return (
+    <>
+      <AdminHeader />
+      <AdminTopics />
+    </>
+  );
+};
+
+export default admin;
