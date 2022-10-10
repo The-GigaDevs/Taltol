1f3ec42d9fb1fcd4b0d489e0efc10a3efaba0324
jawad-ul-hassan <intelligentjawad@gmail.com>
2022-10-11 00:30:40 +0500

    admin-topics and admin-pages author page completed

---
+++ pages/admin-author-page.js
@@ -0,0 +1,15 @@
+import AdminHeader from '../components/admin/AdminHeader';
+import AdminGoBack from '../components/admin/AdminGoBack';
+import AdminAuthorForm from '../components/admin/AdminAuthorForm';
+
+const admin = () => {
+  return (
+    <>
+      <AdminHeader />
+      <AdminGoBack />
+      <AdminAuthorForm />
+    </>
+  );
+};
+
+export default admin;
