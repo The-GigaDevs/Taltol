6373ca5111190bc86fe76f53105808d5860e9864
Atabic <atabic14@gmail.com>
2022-10-23 15:36:25 +0500

    url changes to window.location.href

--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -10,7 +10,7 @@ import {
   } from 'reactjs-social-login';
 import { socialLoginAction } from "../../slices/auth.slice";
 
-const REDIRECT_URI = process.env.NODE_ENV === 'production' ? 'https://www.taltol.com/login' : 'http://localhost:3000/login'
+const REDIRECT_URI = window.location.href;
 
 const SocialLogins = () => {
   const [user, setUser] = useState('');
