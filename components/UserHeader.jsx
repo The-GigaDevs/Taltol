3c77a48f351043a09ad1543942feb1288784b70d
Atabic <atabic14@gmail.com>
2022-10-26 03:17:56 +0500

    collection user changes

--- components/UserHeader.jsx
+++ components/UserHeader.jsx
@@ -1,27 +1,34 @@
 import randomAuthor from '../public/static/quote-card-author.jpg';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const UserHeader = ({ handleTab1, handleTab2, activeTab }) => {
+
+  const user = useSelector(state => state.auth.user)
   return (
     <div className="user-header">
       <div className="container">
         <div className="user-header-content">
           <div className="user-header-content-data">
             <div className="user-header-avatar">
-              <img src={randomAuthor.src} alt="avatar" />
+              <img src={(user?.social_image_url || user?.profile_pic) || randomAuthor.src} alt="avatar" />
             </div>
             <div className="user-header-info">
-              <h2 className="user-header-info-name">Adam Johns</h2>
-              <span className="user-header-info-date">Since Feb, 2020</span>
-              <span className="user-header-info-mail">adam@gmail.com</span>
+              <h2 className="user-header-info-name">
+                {user?.first_name + " " + user?.last_name}
+              </h2>
+              <span className="user-header-info-date">{user?.date_joined}</span>
+              <span className="user-header-info-mail">
+                {user?.email || user?.username}
+              </span>
             </div>
           </div>
           <div className="user-header-content-actions">
             <span
               className={
-                activeTab === 'tab1'
-                  ? 'user-header-content-actions-btn active'
-                  : 'user-header-content-actions-btn'
+                activeTab === "tab1"
+                  ? "user-header-content-actions-btn active"
+                  : "user-header-content-actions-btn"
               }
               onClick={handleTab1}
             >
@@ -29,9 +36,9 @@ const UserHeader = ({ handleTab1, handleTab2, activeTab }) => {
             </span>
             <span
               className={
-                activeTab === 'tab2'
-                  ? 'user-header-content-actions-btn active'
-                  : 'user-header-content-actions-btn'
+                activeTab === "tab2"
+                  ? "user-header-content-actions-btn active"
+                  : "user-header-content-actions-btn"
               }
               onClick={handleTab2}
             >
