0d7ceb7d732b573ac43e490595762685b3716c9b
Atabic <atabic14@gmail.com>
2022-10-30 02:36:46 +0500

    mobile side user data fucntionality added

--- pages/account.js
+++ pages/account.js
@@ -1,10 +1,19 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // warn: Added .jsx extension, as without this the build was failing
 import AccountMobile from "../components/AccountMobile.jsx";
 import MobileMenu from "../components/MobileMenu.jsx";
+import { isUserLoggedIn } from "../slices/auth.slice.js";
 
-const liked = () => {
+const Liked = () => {
+  const dispatch = useDispatch()
+  const authRedux = useSelector(state => state.auth);
+
+  useEffect(() => {
+    dispatch(isUserLoggedIn());
+  }, [authRedux?.isAuthenticated]);
   return (
     <>
       <Head>
@@ -27,4 +36,4 @@ user experience for quotes."
   );
 };
 
-export default liked;
+export default Liked;
