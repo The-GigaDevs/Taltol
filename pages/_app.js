7bf2c949ed650b175192a4b36d2179066ebba9a8
Atabic <atabic14@gmail.com>
2023-01-31 19:23:11 +0500

    Google analytics script added.

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,9 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import store from '../store'
 import { Provider } from 'react-redux';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
@@ -12,6 +13,19 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="shortcut icon" href="/logo.png" />
       </Head>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-YP4840ZZGW" />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YP4840ZZGW');`
+        }}
+      />
       <Component {...pageProps} />
       <ToastContainer
         position="top-right"
